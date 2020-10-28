f1565b0b88b8158891886285b488be908a34f5f9
Alissa Champigny <achampigny4@gmail.com>
2020-10-28 11:44:00 -0400

    employees displaying

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from "react";
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import Employee from './employee';
+
 
 function App() {
+
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    getEmployees();
+
+    console.log("effect run"); //can modify to run at certain points
+  }, []);//set when to run. empty = run once when app starts. w/o empty array it will run many times
+
+  const getEmployees = async () => {
+    const response = await fetch(`https://randomuser.me/api/?results=200&nat=us`);
+    const data = await response.json();
+    console.log(data.results);
+    setEmployees(data.results);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employee Directory</h1>
+      <form className="search-form">
+        <input className="search-bar"/>
+        <button 
+        // onClick={() => }
+        className="search-button" type="submit">Search</button>
+      </form>
+      {employees.map(employee => (
+        <Employee
+        image={employee.picture.thumbnail}
+        fname={employee.name.first}
+        lname={employee.name.last}
+        email={employee.email}
+        phone={employee.cell}/>
+      ))};
     </div>
   );
 }
