667b1dec9107d7610e9259ae61a2c1e428aaa2fd
Alissa Champigny <achampigny4@gmail.com>
2020-10-29 12:13:19 -0400

    employee array repopulates upon search click

--- src/App.js
+++ src/App.js
@@ -6,37 +6,46 @@ import Employee from './employee';
 function App() {
 
   const [employees, setEmployees] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     getEmployees();
-
-    console.log("effect run"); //can modify to run at certain points
-  }, []);//set when to run. empty = run once when app starts. w/o empty array it will run many times
+  }, [query]);//set when to run. empty = run once when app starts.
 
   const getEmployees = async () => {
     const response = await fetch(`https://randomuser.me/api/?results=200&nat=us`);
     const data = await response.json();
-    console.log(data.results);
     setEmployees(data.results);
   }
 
-  return (
-    <div className="App">
-      <h1>Employee Directory</h1>
-      <form className="search-form">
-        <input className="search-bar"/>
-        <button 
-        // onClick={() => }
-        className="search-button" type="submit">Search</button>
-      </form>
-      {employees.map(employee => (
-        <Employee
-        image={employee.picture.thumbnail}
-        fname={employee.name.first}
-        lname={employee.name.last}
-        email={employee.email}
-        phone={employee.cell}/>
-      ))};
+  const updateSearch = event => {
+    setSearch(event.target.value);
+    console.log(search);
+  }
+
+  const getSearch = event => {
+    event.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+ return (
+   <div className="App">
+     <h1>Employee Directory</h1>
+     <form className="search-form" onSubmit={getSearch}>
+       <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+       <button className="search-button" type="submit">Search</button>
+     </form>
+     {employees.map(employee => (
+       <Employee
+       key={employee.id.value}
+       image={employee.picture.thumbnail}
+       fname={employee.name.first}
+       lname={employee.name.last}
+       email={employee.email}
+       phone={employee.cell}/>
+     ))};
     </div>
   );
 }
